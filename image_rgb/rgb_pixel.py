91cb4991baddd5e2d367442457638a5df793f5df
Koray KÖKSAL <88422590+koraykoksal@users.noreply.github.com>
2021-09-09 20:24:26 +0300

    Create rgb_pixel.py

---
+++ image_rgb/rgb_pixel.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+
+
+img = cv2.imread("happy.jpg")
+
+cv2.imshow("Image Size", img)
+
+print(img[(230,80)])
+
+# img değişkeni içindeki resim üzerinden (230,80) değerlerine karşılık gelen pixel sonucu bulunur.
+# ilk parametre yukarıdan aşağıya gelen değer, ikinci parametre soldak sağa gelen değer.
+# iki değerin kesişme noktası pixel bilgisini verir.
+
+
+print("Resmin size bilgisi : "+ str(img.size))  # str tipindeki değer ile oku.size değeri str convert işlemi ile yazdırıldı. Bu işlem str convert yapılmadan da yapılabilir.
+print("resmin veri tibi : " + str(img.dtype))
+print("Resim özellikleri : "+ str(img.shape))
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
